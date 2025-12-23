5f1061290fcf0bf7198e685a64a3a6582af5f4bd
sunrise-oneday <774965688@qq.com>
2025-12-23 20:11:56 +0800

    修改学生加入课程的逻辑

--- VSCode/study_Assistant/src/api/sys/course.js
+++ VSCode/study_Assistant/src/api/sys/course.js
@@ -1,14 +1,12 @@
 import axios from 'axios';
 import config from '@/api/config';
 
+// 获取我的课程列表
+export const getMyCourses = (params) => {
+    return axios.post(config.host + 'course/my-courses', params);
+};
+
 // 加入课程
 export const joinCourse = (params) => {
     return axios.post(config.host + 'course/join', params);
-};
-
-// 获取我的课程列表
-export const getMyCourses = (name) => {
-    return axios.get(config.host + 'course/my-courses', {
-        params: { name: name }
-    });
 };
